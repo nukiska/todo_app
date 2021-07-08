3209a2788e14266b6aa484fcfa429b17560645dd
nukiska <g.nina@seznam.cz>
2021-07-08 14:06:51 +0200

    created Task model in models.py

--- todo_app/todo_list/models.py
+++ todo_app/todo_list/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['completed', 'deadline']
