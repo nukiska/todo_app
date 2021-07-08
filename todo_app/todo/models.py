224d40b63aeb58881532e3bf8229cc39ae51fadb
nukiska <g.nina@seznam.cz>
2021-07-08 14:50:47 +0200

    rename todo_list app to todo app, modified deadline at Task model

--- todo_app/todo/models.py
+++ todo_app/todo/models.py
@@ -8,7 +8,7 @@ class Task(models.Model):
     description = models.TextField(null=True, blank=True)
     completed = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
+    deadline = models.DateField(auto_now_add=False, auto_now=False, null=True, blank=True)
 
     def __str__(self):
         return self.title
