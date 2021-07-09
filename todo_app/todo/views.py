2ea6232ae96dfd4e397c994cbde36eb9df839734
nukiska <g.nina@seznam.cz>
2021-07-09 14:12:04 +0200

    modified context data of TaskList class in views.py

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -9,6 +9,12 @@ class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(completed=False).count()
+        return context
+
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
