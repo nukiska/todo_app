7c8a75edfd8cd467fa1475334ae4b50927ae3739
nukiska <g.nina@seznam.cz>
2021-07-22 00:38:29 +0200

    timeline created and modified max length of task

--- todo_app/todo/models.py
+++ todo_app/todo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     completed = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
