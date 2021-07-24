ce7c360156e342b671e333484bef739ec7c41c25
nukiska <g.nina@seznam.cz>
2021-07-25 00:00:28 +0200

    set STATIC_ROOT and STATICFILES_DIRS in settings.py

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,6 +124,8 @@ LOGIN_REDIRECT_URL = 'tasks'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
