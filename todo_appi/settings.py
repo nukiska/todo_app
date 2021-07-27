13eed0567d37f491160f9b73fabc74ae22db96e3
nukiska <g.nina@seznam.cz>
2021-07-28 01:45:57 +0200

    folder todo_app/ deleted

--- todo_appi/settings.py
+++ todo_appi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
 import django_heroku
+from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(nzb7-58tp!!f++1f__3(1$^4v+)slxc8d@0lm*fsa&+iq%v6('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
